d599d5b31db4728b576f5e3d3601a4fe091a805d
mezdahun <mezeydavid@gmail.com>
2021-11-19 17:05:18 +0100

    added first batch of files for testing purposes on multiple nVidia boards

---
+++ aseba.py
@@ -0,0 +1,20 @@
+import dbus
+import dbus.mainloop.glib
+from time import sleep
+
+dbus.mainloop.glib.threads_init()
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SessionBus()
+
+# Create Aseba network
+# if network is None:
+network = dbus.Interface(bus.get_object('ch.epfl.mobots.Aseba', '/'),
+                         dbus_interface='ch.epfl.mobots.AsebaNetwork')
+network.SetVariable("thymio-II", "motor.left.target", [-100])
+network.SetVariable("thymio-II", "motor.right.target", [100])
+sleep(4)
+network.SetVariable("thymio-II", "motor.left.target", [100])
+network.SetVariable("thymio-II", "motor.right.target", [-100])
+sleep(4)
+network.SetVariable("thymio-II", "motor.left.target", [0])
+network.SetVariable("thymio-II", "motor.right.target", [0])
