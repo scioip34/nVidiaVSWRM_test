01d95ae037dda78a81d7da18a191c252551334df
mezdahun <mezeydavid@gmail.com>
2022-04-05 12:25:57 +0200

    merge inference and camera

--- image_client.py
+++ image_client.py
@@ -514,10 +514,10 @@ if __name__ == '__main__':
 
             t1 = datetime.datetime.now()
             print(f"inf time: {(t1 - t0).total_seconds()}")
-            print(responses[-1].get_response().id)
-            # (results, error) = user_data._completed_requests.get()
-            # t2 = datetime.datetime.now()
-            # print(f"ret time: {(t2 - t1).total_seconds()}")
+            #print(responses[-1].get_response().id)
+            (results, error) = user_data._completed_requests.get()
+            t2 = datetime.datetime.now()
+            print(f"ret time: {(t2 - t1).total_seconds()}")
             # print("Retrieved response with id: ", results.get_response().id)
 
             k = cv2.waitKey(1) & 0xFF
