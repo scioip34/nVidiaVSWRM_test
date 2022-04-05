9071076ffdc950765b30b694d25a9ca0c8c372f1
mezdahun <mezeydavid@gmail.com>
2022-04-05 11:31:02 +0200

    merge inference and camera

--- image_client.py
+++ image_client.py
@@ -511,7 +511,7 @@ if __name__ == '__main__':
             (results, error) = user_data._completed_requests.get()
             print("Retrieved response with id: ", results.get_response().id)
 
-            cv2.imshow("CSI Camera", np.asarray(img_annotated))
+            cv2.imshow("CSI Camera", img)
             k = cv2.waitKey(1) & 0xFF
             if k == ord("q"):
                 # ESC pressed
