038fd8afa83944f4c162453f73347b89a7a97458
mezdahun <mezeydavid@gmail.com>
2022-04-06 16:30:24 +0200

    add first calibration test

--- realtime_test_fisheye_calibration.py
+++ realtime_test_fisheye_calibration.py
@@ -82,6 +82,7 @@ def show_camera():
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print(gstreamer_pipeline(flip_method=2))
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
     if cap.isOpened():
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
         # Window
