a93192df94faa55f1e77769ee5eb043daa7549e0
mezdahun <mezeydavid@gmail.com>
2022-04-14 11:50:09 +0200

    Prepared for distribution

--- omnidir_calibrate.py
+++ omnidir_calibrate.py
@@ -1,22 +1,43 @@
 """
-Rinning this script needs opencv-contrib-python instead of the normal opencv-python to get the omnidir module.
-Before installing via pip uninstall other cv2 versions via pip uninstall opencv-python.
+Script to calibrate ultra-wide FOV fisheye lenses using opencv-contrib-python's omnilib submodule.
+
+Running this script needs opencv-contrib-python instead of the normal opencv-python to get the omnidir module.
+Before installing it via 'pip install opencv-contrib-python',
+uninstall other cv2 versions via 'pip uninstall opencv-python'.
+
+This script only works for cylindrical reconstruction. For other rectification types the new_K camera matrix needs to be
+defined differently.
+
+Calibration can happen via jpg images in the data folder or if the calibration points have been previously extracted
+and saved as npz files, via npz files from the data folder.
+
+created by mezdahun
+14.04.2022
 """
-import cv2
-
-assert cv2.__version__[0] == '4', 'The fisheye module requires opencv version >= 3.0.0'
-import numpy as np
-import glob
-
+import json
 import signal
+import cv2
+import glob
+import numpy as np
 from contextlib import contextmanager
 
+assert cv2.__version__[0] == '4', 'The fisheye module requires opencv version >= 3.0.0'
+
+
+# Checkerboard stuff is prone to get lazy, we introduce timeout
 class TimeoutException(Exception): pass
 
+
+# Signal handler for timeout
 def signal_handler(signum, frame):
     raise TimeoutException("Timed out!")
+
+
+# Connecting alarm signals to handler
 signal.signal(signal.SIGALRM, signal_handler)
 
+
+# Introducing possible context for using timeout
 @contextmanager
 def time_limit(seconds):
     signal.setitimer(signal.ITIMER_REAL, seconds)
@@ -25,17 +46,26 @@ def time_limit(seconds):
     finally:
         signal.setitimer(signal.ITIMER_REAL, 0)
 
+
+# Checkerboard dimensions (inner crossing points)
 CHECKERBOARD = (6, 9)
+# Calibration stopping criteria
 subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30000, 0.001)
-calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv2.fisheye.CALIB_FIX_SKEW
+# Object points for calibration
 objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
 objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+
 _img_shape = None
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
+
+# If previously found calibration points are already exported as npz files
 pointsets = glob.glob('./data/calib_pointset_*.npz')
+# If npz files are not found but jpg calibration images are available
 images = glob.glob('./data/*.jpg')
+
 if len(pointsets) == 0:
+    # Did not found npz files
     for fname in images:
         print(f"Processing image: ", fname)
         img = cv2.imread(fname)
@@ -47,7 +77,7 @@ if len(pointsets) == 0:
         # Find the chess board corners
         print("Finding Checkerboard...")
         try:
-            with time_limit(1):
+            with time_limit(3):
                 ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD,
                                                          cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
         except TimeoutException as e:
@@ -57,21 +87,19 @@ if len(pointsets) == 0:
         # If found, add object points, image points (after refining them)
         if ret == True:
             # print("Found calibration points!")
-            print(objp.dtype)
             objpoints.append(objp)
             cv2.cornerSubPix(gray, corners, (3, 3), (-1, -1), subpix_criteria)
             imgpoints.append(corners)
         else:
             print("Didn't find calibration points!")
 else:
-    print("Found previously exported pointsets, will load calibration points from these")
+    print("Found previously exported pointsets, will load calibration points from these!")
     img = cv2.imread(images[0])
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _img_shape = img.shape[:2]
     for pointset_path in pointsets:
         print(f"Loading points from {pointset_path}")
         calibration_points = np.load(pointset_path)
-        print(calibration_points["objectPointsArray"].dtype)
         imgPointsArray = list(calibration_points["imgPointsArray"].astype(np.float32))
         imgpoints.extend(imgPointsArray)
         objectPointsArray = list(calibration_points["objectPointsArray"].astype(np.float32))
@@ -82,49 +110,21 @@ else:
     objpoints = newobjpoints
 
 N_OK = len(objpoints)
-print(len(objpoints))
-print(objpoints[0].shape)
-print(imgpoints[0].shape)
 K = np.zeros((3, 3))
 D = np.zeros((4, 1))
 rvecs = [np.zeros((1, 1, 3), dtype=np.float32) for i in range(N_OK)]
 tvecs = [np.zeros((1, 1, 3), dtype=np.float32) for i in range(N_OK)]
-# rms, _, _, _, _ = \
-#     cv2.fisheye.calibrate(
-#         objpoints,
-#         imgpoints,
-#         gray.shape[::-1],
-#         K,
-#         D,
-#         rvecs,
-#         tvecs,
-#         calibration_flags,
-#         (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
-#     )
-calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
-# xi = np.zeros(1)
-# rms, _, _, _, _, _ = \
-#     cv2.omnidir.calibrate(
-#         objpoints,
-#         imgpoints,
-#         gray.shape[::-1],
-#         K,
-#         xi,
-#         D,
-#         calibration_flags,
-#         (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1),
-#         rvecs,
-#         tvecs
-#     )
+
+calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv2.fisheye.CALIB_CHECK_COND + cv2.fisheye.CALIB_FIX_SKEW
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-rms, K, xi, D, rvecs, tvecs, idx  =  cv2.omnidir.calibrate(
-            objectPoints=objpoints,
-            imagePoints=imgpoints,
-            size=gray.shape[::-1],
-            K=None, xi=None, D=None, rvecs=rvecs, tvecs=tvecs,
-            flags=cv2.omnidir.CALIB_FIX_K1,#cv2.omnidir.CALIB_FIX_K1 + cv2.omnidir.CALIB_FIX_K2 + cv2.omnidir.CALIB_USE_GUESS + cv2.omnidir.CALIB_FIX_SKEW + cv2.omnidir.CALIB_FIX_CENTER + cv2.omnidir.CALIB_FIX_P2,
-            criteria=subpix_criteria)
+rms, K, xi, D, rvecs, tvecs, idx = cv2.omnidir.calibrate(
+    objectPoints=objpoints,
+    imagePoints=imgpoints,
+    size=gray.shape[::-1],
+    K=None, xi=None, D=None, rvecs=rvecs, tvecs=tvecs,
+    flags=cv2.omnidir.CALIB_FIX_K1,
+    criteria=subpix_criteria)
 
 print("Found " + str(N_OK) + " valid data points for calibration")
 print("DIM=" + str(_img_shape[::-1]))
@@ -132,81 +132,66 @@ print("K=np.array(" + str(K.tolist()) + ")")
 print("D=np.array(" + str(D.tolist()) + ")")
 
 DIM = _img_shape[::-1]
-
-# DIM=(1640, 1232)
-# K=np.array([[630.1409550612487, 0.0, 820.0], [0.0, 630.1631329394177, 616.0], [0.0, 0.0, 1.0]])
-# D=np.array([[-0.006845095123421685, 0.0007987746751401334, 1.7327659142422208e-05, 0.00016330173727187834]])
-
-balance = 1
-
-dim2 = (2000, 1000)
-dim3 = None
-
-img = cv2.imread(images[-10])
+img = cv2.imread(images[0])
 dim1 = img.shape[:2][::-1]  # dim1 is the dimension of input image to un-distort
 assert dim1[0] / dim1[1] == DIM[0] / DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in " \
                                              "calibration "
-if not dim2:
-    dim2 = dim1
-if not dim3:
-    dim3 = dim1
-scaled_K = K * dim2[0] / DIM[0]  # The values of K is to scale with image dimension.
-scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
-# This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image. OpenCV document
-# failed to make this clear!
-new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
-#map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
-# print("dim2", (int(dim3[0]*1), int(dim3[1]*1)))
-# map1, map2 = cv2.omnidir.initUndistortRectifyMap(scaled_K, D, xi, np.eye(3), new_K, dim2, cv2.CV_32FC1, 2)
-# undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
+# dim2 is the desired image size after undistortion
+dim2 = (2000, 1000)
+
+# Edge size is to include all the image and reduce cv2 rectification on the edges. This should be adjusted according
+# to dim2. If too much is visible on the edges (double images) you should reduce it, if too less (rectified image)
+# increase it.
+edge_size = 200
+# New desired camera matrix that changes according to rectification type (e.g. cylindrical)
+new_K = np.array([[(dim2[0] - 2 * edge_size) / 3.1415, 0, (dim2[0] - edge_size)],
+                  [0.0, (dim2[1]) / 3.1415, dim2[1] / 2],
+                  [0.0, 0.0, 1.0]])
+
+# Rotation matrix to be applied within remapping
+R = np.array([[1, 0, 0],
+              [0, 0, -1],
+              [0, 1, 0]], dtype=np.float32)
+
+# Data summary
 data = {'dim1': dim1,
         'dim2': dim2,
-        'dim3': dim3,
         'K': np.asarray(K).tolist(),
         'D': np.asarray(D).tolist(),
         'xi': np.asarray(xi).tolist(),
         'new_K': np.asarray(new_K).tolist(),
-        'scaled_K': np.asarray(scaled_K).tolist(),
-        'balance': balance}
-from pprint import pprint
-import json
-pprint(data)
+        'R': np.asarray(R).tolist()}
 
 with open("fisheye_calibration_data.json", "w") as f:
     json.dump(data, f)
-cv2.namedWindow("original", cv2.WINDOW_NORMAL)
-cv2.imshow("original", img)
-# new_K = np.array([[1000/3.1415, 0, 0],
-#                 [0, 250/3.1415,  0],
-#                 [0, 0, 1]])
 
-# new_K = np.array([[2000/4, 0, 2000/2],
-#                 [0, 500/4, 500/2],
-#                 [0, 0, 1]])
-
-new_K = np.array([[(dim2[0]-400)/3.1415, -0.86, (dim2[0]-200)],
-          [0.0, (dim2[1])/3.1415, dim2[1]/2],
-          [0.0, 0.0, 1.0]])
-R = np.array([[1, 0, 0],
-              [0, 0, -1],
-              [0, 1, 0]], dtype=np.float32)
-#D = np.array([[0, 0.62983775417965, -0.0026918796324266934, 0.0111223958104562154]])
-#xi = np.array([[2.19908895]])
-
-map1, map2 = cv2.omnidir.initUndistortRectifyMap(K, D, xi, R, new_K, dim2, cv2.CV_32FC1, 2)
+# Creating and saving maps
+map1, map2 = cv2.omnidir.initUndistortRectifyMap(K, D, xi, R, new_K, dim2, cv2.CV_32FC1,
+                                                 cv2.omnidir.RECTIFY_CYLINDRICAL)
 np.savez("maps.npz", map1=map1, map2=map2)
 
-maps = np.load("maps.npz")
-map1 = maps['map1']
-map2 = maps['map2']
+# Maps can be created on any hardware and be loaded later on the edge where opencv-contrib is not available
+# path_to_maps = "maps.npz"
+# maps = np.load(path_to_maps)
+# map1 = maps['map1']
+# map2 = maps['map2']
+
+# Remapping original image
 undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
-#undistorted_img = cv2.omnidir.undistortImage(img, K, D, xi, cv2.omnidir.RECTIFY_CYLINDRICAL, Knew=new_K, R=R, new_size = dim2) #  @param flags Flags indicates the rectification type,  RECTIFY_PERSPECTIVE, RECTIFY_CYLINDRICAL, RECTIFY_LONGLATI and RECTIFY_STEREOGRAPHIC
+# Alternative to init and remap but much slower and needs to have opencv-contrib installed @param flags Flags
+# indicates the rectification type,  RECTIFY_PERSPECTIVE, RECTIFY_CYLINDRICAL, RECTIFY_LONGLATI and
+# RECTIFY_STEREOGRAPHIC undistorted_img = cv2.omnidir.undistortImage(img, K, D, xi, cv2.omnidir.RECTIFY_CYLINDRICAL,
+# Knew=new_K, R=R, new_size = dim2)
 
+# Showing original image
+cv2.namedWindow("original", cv2.WINDOW_NORMAL)
+cv2.imshow("original", img)
 
+# Showing undistorted image
 cv2.namedWindow("undistorted", cv2.WINDOW_NORMAL)
-#undistorted_img = cv2.resize(undistorted_img, (int(dim2[0]/3), int(dim2[1]/3)))
 cv2.imshow("undistorted", undistorted_img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
